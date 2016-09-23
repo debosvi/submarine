39359e9ba64eb0a0426d875d6950d849bb39ba04
Vincent de RIBOU <belzo2005-dolphin@yahoo.fr>
2016-09-23 13:04:40 +0200

    Build toolchain aboriginal tools

--- tools/toolchains/cross-build.sh
+++ tools/toolchains/cross-build.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
-# i686-elf-tools.sh
+# cross-build.sh
 # v1.0
 
-BINUTILS_VERSION=2.27
-GCC_VERSION=6.2.0
-GDB_VERSION=7.11
-MPFR_VERSION=3.1.4
-GMP_VERSION=6.1.1
-MPC_VERSION=1.0.3
-ISL_VERSION=0.17
-NEWLIB_VERSION=2.4.0
-
-BINUTILS_ARCHIVE=binutils-$BINUTILS_VERSION.tar.bz2
-GCC_ARCHIVE=gcc-$GCC_VERSION.tar.bz2
-GDB_ARCHIVE=gdb-$GDB_VERSION.tar.gz
-MPFR_ARCHIVE=mpfr-$MPFR_VERSION.tar.bz2
-GMP_ARCHIVE=gmp-$GMP_VERSION.tar.bz2
-MPC_ARCHIVE=mpc-$MPC_VERSION.tar.gz
-ISL_ARCHIVE=isl-$ISL_VERSION.tar.gz
-NEWLIB_ARCHIVE=newlib-$NEWLIB_VERSION.tar.gz
+ABORIGINAL_VERSION=1.4.5
+ABORIGINAL_NAME=aboriginal-$ABORIGINAL_VERSION
 
 NB_CPU=`grep -c ^processor /proc/cpuinfo`
 #export PARALLEL_BUILD_OPTS=-j$NB_CPU
@@ -48,160 +33,69 @@ SRC_DIR=$SUBMARINE_BUILD_DIR/toolchains/src
 TGT_DIR=$SUBMARINE_BUILD_DIR/toolchains/target
 INSTALL_PREFIX=$SUBMARINE_SYSROOT_DIR
 
-# cd $BASEDIR
-# git clone https://github.com/mxe/mxe.git
-# cd mxe
-# make gcc
+function die {
+	echo -e $1
+	exit 1
+} 
 
 function extract_archive {
-    dir=`echo $1  | sed -e "s/^\(.*\)\.tar.*$/\1/g"`
-    if [ ! -d $SRC_DIR/$dir ]; then
-        mkdir -p $SRC_DIR
-	echo -e "Extract $dir"
-        tar -xf $DL_DIR/$1 -C $SRC_DIR
-    fi
+	dir=`echo $1  | sed -e "s/^\(.*\)\.tar.*$/\1/g"`
+   if [ ! -d $SRC_DIR/$dir ]; then
+		mkdir -p $SRC_DIR
+		echo -e "Extract $dir"
+		tar -xf $DL_DIR/$1 -C $SRC_DIR
+	fi
 }
 
 function download_archive {
-    archive=`basename $1`
-    url=`echo $1 | sed -e "s/^\(.*\)\/.*$/\1/g"`
-    ver=`echo $1 | cut -d"-" -f2-`
-    if [ ! -f $DL_DIR/$archive ]; then
-        mkdir -p $DL_DIR
-	echo -e "Download $archive"
-        wget --spider -q $1 &&
-			wget -c $1 -O $DL_DIR/$archive || 
-			wget -c $url/v$ver -O $DL_DIR/$archive || exit 1
-    fi
-    extract_archive $archive
+	url=$1
+	name=$2
+	ver=`echo $name | cut -d"-" -f2-`
+	if wget --spider -q $url/$name.tar.xz; then 
+		archive=$name.tar.xz
+	elif wget --spider -q $url/v$ver.tar.xz; then 
+		archive=v$ver.tar.xz
+	elif wget --spider -q $url/$name.tar.bz2; then 
+		archive=$name.tar.bz2
+	elif wget --spider -q $url/v$ver.tar.bz2; then 
+		archive=v$ver.tar.bz2
+	elif wget --spider -q $url/$name.tar.gz; then 
+		archive=$name.tar.gz
+	elif wget --spider -q $url/v$ver.tar.gz; then 
+		archive=v$ver.tar.gz
+	else
+		die "No archive to be found!!"
+	fi
+	if [ ! -f $DL_DIR/.dl_$name ]; then
+		mkdir -p $DL_DIR
+		echo -e "Download $name"
+		
+		wget -q -c $url/$archive -O $DL_DIR/$archive || die "No archive downloaded!!"
+		touch $DL_DIR/.dl_$name
+	fi
+	extract_archive $archive
 }
 
 # Downloads
 
-download_archive http://ftp.gnu.org/gnu/binutils/$BINUTILS_ARCHIVE
-download_archive http://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/$GCC_ARCHIVE
-download_archive http://ftp.gnu.org/gnu/gdb/$GDB_ARCHIVE
-download_archive http://www.mpfr.org/mpfr-current/$MPFR_ARCHIVE
-download_archive http://ftp.gnu.org/gnu/gmp/$GMP_ARCHIVE
-download_archive http://ftp.gnu.org/gnu/mpc/$MPC_ARCHIVE
-download_archive http://isl.gforge.inria.fr/$ISL_ARCHIVE
-download_archive https://github.com/openrisc/newlib/archive/$NEWLIB_ARCHIVE
+download_archive http://landley.net/aboriginal/downloads $ABORIGINAL_NAME
 
-
-# Automatically download GMP, MPC and MPFR. These will be placed into the right directories.
-# You can also download these separately, and specify their locations as arguments to ./configure
-cd $SRC_DIR/gcc-$GCC_VERSION
-rm -rfv mpfr gmp mpc isl
-ln -sfn $SRC_DIR/mpfr-$MPFR_VERSION mpfr
-ln -sfn $SRC_DIR/gmp-$GMP_VERSION gmp
-ln -sfn $SRC_DIR/mpc-$MPC_VERSION mpc
-ln -sfn $SRC_DIR/isl-$ISL_VERSION isl
-ln -sfn $SRC_DIR/newlib-$NEWLIB_VERSION newlib
-
-## for qemu i686
-TARGET=i686-elf
-## for qemu PIC32
-# TARGET=mipsel-elf32
+## default i686
+CROSS_TARGET=${CROSS_TARGET:-i686}
 
 # Binutils
-cd $BASEDIR
-mkdir -p $TGT_DIR/$TARGET/binutils-$BINUTILS_VERSION
-cd $TGT_DIR/$TARGET/binutils-$BINUTILS_VERSION
-if [ ! -f .configure ]; then
-    $SRC_DIR/binutils-$BINUTILS_VERSION/configure --prefix=$INSTALL_PREFIX --target=$TARGET --with-sysroot --enable-interwork --enable-multilib --disable-nls --disable-werror
-    if [ $? -eq 1 ]; then  
-		echo -e "Binutils configure failed!" && exit 1
-	fi
-    touch .configure
-fi
-if [ ! -f .install ]; then
-    make $PARALLEL_BUILD_OPTS
-    if [ $? -eq 1 ]; then  
-		echo -e "Binutils build failed!" && exit 1
-    fi
-    make install
-    if [ $? -eq 1 ]; then  
-		echo -e "Binutils install failed!" && exit 1
-	fi
-    touch .install
+# cd $BASEDIR
+cd $SRC_DIR/$ABORIGINAL_NAME
+if [ ! -f .build ]; then
+	cp -av $BASEDIR/configs/aboriginal.config config
+ 	./download.sh $CROSS_TARGET || die "Aboriginal toolchain download failed!"
+	./host-tools.sh || die "Aboriginal toolchain host tools build failed!"
+	./simple-cross-compiler.sh $CROSS_TARGET || die "Aboriginal toolchain build failed!"
+	touch .build
 fi
 
-
-# GCC
-cd $BASEDIR
-mkdir -p $TGT_DIR/$TARGET/gcc-$GCC_VERSION
-cd $TGT_DIR/$TARGET/gcc-$GCC_VERSION
-if [ ! -f .configure ]; then
-    $SRC_DIR/gcc-$GCC_VERSION/configure --prefix=$INSTALL_PREFIX --target=$TARGET --enable-interwork --disable-multilib --disable-nls --enable-languages=c,c++ --without-headers
-    if [ $? -eq 1 ]; then  
-		echo -e "GCC configure failed!" && exit 1
-    fi
-    touch .configure
+if [ ! -f $SUBMARINE_SYSROOT_DIR/.install ]; then
+	mkdir $SUBMARINE_SYSROOT_DIR
+	tar -xf build/simple-cross-compiler-$CROSS_TARGET.tar.gz -C $SUBMARINE_SYSROOT_DIR --strip-components=1 || die "Aboriginal not ready to use!"
+	touch $SUBMARINE_SYSROOT_DIR/.install
 fi
-if [ ! -f .install ]; then
-    make $PARALLEL_BUILD_OPTS all-gcc
-    if [ $? -eq 1 ]; then  
-		echo -e "GCC build failed!" && exit 1
-    fi
-    make $PARALLEL_BUILD_OPTS all-target-libgcc
-    if [ $? -eq 1 ]; then  
-		echo -e "GCC libgcc build failed!" && exit 1
-    fi
-    make install-gcc
-    if [ $? -eq 1 ]; then  
-		echo -e "GCC install failed!" && exit 1
-    fi
-    make install-target-libgcc
-    if [ $? -eq 1 ]; then  
-		echo -e "GCC libgcc install failed!" && exit 1
-	fi
-    touch .install
-fi
-
-# newlib
-#cd $BASEDIR
-#mkdir -p $TGT_DIR/$TARGET/newlib-$NEWLIB_VERSION
-#cd $TGT_DIR/$TARGET/newlib-$NEWLIB_VERSION
-#if [ ! -f .configure ]; then
-#    $SRC_DIR/newlib-$NEWLIB_VERSION/configure --prefix=$INSTALL_PREFIX --target=$TARGET --enable-interwork --enable-multilib --disable-nls
-#	if [ $? -eq 1 ]; then  
-#		echo -e "newlib configure failed!" && exit 1
-#    fi
-#    touch .configure
-#fi
-#if [ ! -f .install ]; then
-#    make $PARALLEL_BUILD_OPTS all
-#	if [ $? -eq 1 ]; then  
-#		echo -e "newlib build failed!" && exit 1
-#    fi
-#    make install
-#	if [ $? -eq 1 ]; then  
-#		echo -e "newlib install failed!" && exit 1
-#    fi
-#    touch .install
-#fi
-#
-
-# GDB
-cd $BASEDIR
-mkdir -p $TGT_DIR/$TARGET/gdb-$GDB_VERSION
-cd $TGT_DIR/$TARGET/gdb-$GDB_VERSION
-if [ ! -f .configure ]; then
-    $SRC_DIR/gdb-$GDB_VERSION/configure --prefix=$INSTALL_PREFIX --target=$TARGET --disable-nls --disable-werror
-	if [ $? -eq 1 ]; then  
-		echo -e "GDB configure failed!" && exit 1
-    fi
-    touch .configure
-fi
-if [ ! -f .install ]; then
-    make $PARALLEL_BUILD_OPTS
-	if [ $? -eq 1 ]; then  
-		echo -e "GDB build failed!" && exit 1
-    fi
-    make install
-	if [ $? -eq 1 ]; then  
-		echo -e "GDB install failed!" && exit 1
-    fi
-    touch .install
-fi
-
