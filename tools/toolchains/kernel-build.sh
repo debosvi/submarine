831e6701ec8f053a92e56e078f23c8ba12e96d80
Vincent de RIBOU <belzo2005-dolphin@yahoo.fr>
2016-09-28 12:14:01 +0200

    Fix building rootfs cross compiling

--- tools/toolchains/kernel-build.sh
+++ tools/toolchains/kernel-build.sh
@@ -1,105 +1,50 @@
-#!/bin/bash
+#!/bin/bash -x
 # kernel-build.sh
 # v1.0
 
+. `dirname $0`/basic-build.sh
+
+KERNEL_BRANCH=v4.x
 KERNEL_VERSION=4.3
 KERNEL_NAME=linux-$KERNEL_VERSION
-KERNEL_ARCHIVE=$KERNEL_NAME.tar.xz
 
-NB_CPU=`grep -c ^processor /proc/cpuinfo`
-export PARALLEL_BUILD_OPTS=-j$NB_CPU
-
-set -e
-
-if [ "$SUBMARINE_PROJECT_DIR" == "" ]; then
-    echo -e "Must init project environment"
-    echo -e "Must set env SUBMARINE_PROJECT_DIR variable"
-    exit 1
-fi
-
-if [ "$SUBMARINE_SYSROOT_DIR" == "" ]; then
-    echo -e "Must set env SUBMARINE_SYSROOT_DIR variable"
-    exit 1
-fi
-
-if [ "$SUBMARINE_BUILD_DIR" == "" ]; then
-    echo -e "Must set env SUBMARINE_BUILD_DIR variable"
-    exit 1
-fi
-
-BASEDIR=$SUBMARINE_PROJECT_DIR
-DL_DIR=$BASEDIR/dl
-RFS_DIR=$SUBMARINE_BUILD_DIR/rootfs
-SRC_DIR=$SUBMARINE_BUILD_DIR/toolchains/src
-TGT_DIR=$SUBMARINE_BUILD_DIR/toolchains/target
-INSTALL_PREFIX=$SUBMARINE_SYSROOT_DIR
-
-function die {
-	echo -e $1
-	exit 1
-}
-
-function extract_archive {
-    dir=`echo $1  | sed -e "s/^\(.*\)\.tar.*$/\1/g"`
-    if [ ! -d $SRC_DIR/$dir ]; then
-        mkdir -p $SRC_DIR
-	echo -e "Extract $dir"
-        tar -xf $DL_DIR/$1 -C $SRC_DIR
-    fi
-}
-
-function download_archive {
-    archive=`basename $1`
-    url=`echo $1 | sed -e "s/^\(.*\)\/.*$/\1/g"`
-    ver=`echo $1 | cut -d"-" -f2-`
-    if [ ! -f $DL_DIR/$archive ]; then
-        mkdir -p $DL_DIR
-	echo -e "Download $archive"
-        wget --spider -q $1 &&
-			wget -c $1 -O $DL_DIR/$archive || 
-			wget -c $url/v$ver -O $DL_DIR/$archive || exit 1
-    fi
-    extract_archive $archive
-}
 
 # Downloads
 
-download_archive https://www.kernel.org/pub/linux/kernel/v4.x/$KERNEL_ARCHIVE
-
-## for qemu i686
-ARCH=i686
-TARGET=i686-pc-linux
-## for qemu PIC32
-# TARGET=mipsel-elf32
+download_archive https://www.kernel.org/pub/linux/kernel/$KERNEL_BRANCH $KERNEL_NAME
 
 # kernel
 cd $BASEDIR
-mkdir -p $TGT_DIR/$TARGET/$KERNEL_NAME/{build,rootfs}
 
-cd $TGT_DIR/$TARGET/$KERNEL_NAME/build
+mkdir -p $TGT_DIR/$CROSS_TARGET/$KERNEL_NAME/{build,rootfs}
+cd $TGT_DIR/$CROSS_TARGET/$KERNEL_NAME/build
+
+DEFAULT_MAKE_OPTS="ARCH=x86 CROSS_COMPILE=$CROSS_COMPILE $PARALLEL_BUILD_OPTS"
+
 if [ ! -f .configure ]; then
     cp -av $BASEDIR/configs/kernel.config $PWD/.config || die "Unable to place default config";
-    make $PARALLEL_BUILD_OPTS -C $SRC_DIR/$KERNEL_NAME O=$PWD oldconfig  || die "Unable to initiate kernel";
+    make $DEFAULT_MAKE_OPTS -C $SRC_DIR/$KERNEL_NAME O=$PWD oldconfig  || die "Unable to initiate kernel";
     touch .configure
 fi
 if [ ! -f .build ]; then
-    make $PARALLEL_BUILD_OPTS bzImage || die "Unable to build kernel image";
-    make $PARALLEL_BUILD_OPTS modules || die "Unable to build kernel modules";
+    make $DEFAULT_MAKE_OPTS bzImage || die "Unable to build kernel image";
+    make $DEFAULT_MAKE_OPTS modules || die "Unable to build kernel modules";
     touch .build
 fi
 if [ ! -f .install ]; then
-    rm -rf $TGT_DIR/$TARGET/$KERNEL_NAME/rootfs || die "Unable to clean kernel rootfs";
+    rm -rf $TGT_DIR/$CROSS_TARGET/$KERNEL_NAME/rootfs || die "Unable to clean kernel rootfs";
     
-    mkdir $TGT_DIR/$TARGET/$KERNEL_NAME/rootfs || die "Unable to create kernel rootfs base";
-    make INSTALL_PATH=$TGT_DIR/$TARGET/$KERNEL_NAME/rootfs install || die "Unable to install kernel image";
-    make INSTALL_MOD_PATH=$TGT_DIR/$TARGET/$KERNEL_NAME/rootfs modules_install || die "Unable to install kernel modules";
-    rm -rfv $TGT_DIR/$TARGET/$KERNEL_NAME/rootfs/lib/modules/$KERNEL_VERSION.0/{source,build} || die "Unable to clean build install modules tree";
+    mkdir $TGT_DIR/$CROSS_TARGET/$KERNEL_NAME/rootfs || die "Unable to create kernel rootfs base";
+    make $DEFAULT_MAKE_OPTS INSTALL_PATH=$TGT_DIR/$CROSS_TARGET/$KERNEL_NAME/rootfs install || die "Unable to install kernel image";
+    make $DEFAULT_MAKE_OPTS INSTALL_MOD_PATH=$TGT_DIR/$CROSS_TARGET/$KERNEL_NAME/rootfs modules_install || die "Unable to install kernel modules";
+    rm -rfv $TGT_DIR/$CROSS_TARGET/$KERNEL_NAME/rootfs/lib/modules/$KERNEL_VERSION.0/{source,build} || die "Unable to clean build install modules tree";
     touch .install
 fi
+
 if [ ! -f .archive ]; then
     archive=$SUBMARINE_BUILD_DIR/kernel.tar.gz
     rm -rf $archive
-    tar -C $TGT_DIR/$TARGET/$KERNEL_NAME/ -cjf $archive rootfs || die "Unable to archive kernel install tree";
+    tar -C $TGT_DIR/$CROSS_TARGET/$KERNEL_NAME/ -cjf $archive rootfs || die "Unable to archive kernel install tree";
     touch .archive
 fi
 
--- tools/toolchains/toybox-build.sh
+++ tools/toolchains/toybox-build.sh
@@ -1,101 +1,43 @@
-#!/bin/bash
+#!/bin/bash -x
 # toybox-build.sh
 # v1.0
+
+. `dirname $0`/basic-build.sh
+
 TOYBOX_VERSION=0.7.1
 TOYBOX_NAME=toybox-$TOYBOX_VERSION
-TOYBOX_ARCHIVE=$TOYBOX_NAME.tar.gz
-
-NB_CPU=`grep -c ^processor /proc/cpuinfo`
-export PARALLEL_BUILD_OPTS=-j$NB_CPU
-
-set -e
-
-if [ "$SUBMARINE_PROJECT_DIR" == "" ]; then
-    echo -e "Must init project environment"
-    echo -e "Must set env SUBMARINE_PROJECT_DIR variable"
-    exit 1
-fi
-
-if [ "$SUBMARINE_SYSROOT_DIR" == "" ]; then
-    echo -e "Must set env SUBMARINE_SYSROOT_DIR variable"
-    exit 1
-fi
-
-if [ "$SUBMARINE_BUILD_DIR" == "" ]; then
-    echo -e "Must set env SUBMARINE_BUILD_DIR variable"
-    exit 1
-fi
-
-BASEDIR=$SUBMARINE_PROJECT_DIR
-DL_DIR=$BASEDIR/dl
-RFS_DIR=$SUBMARINE_BUILD_DIR/rootfs
-SRC_DIR=$SUBMARINE_BUILD_DIR/toolchains/src
-TGT_DIR=$SUBMARINE_BUILD_DIR/toolchains/target
-INSTALL_PREFIX=$SUBMARINE_SYSROOT_DIR
-
-function die {
-	echo -e $1
-	exit 1
-}
-
-function extract_archive {
-    dir=`echo $1  | sed -e "s/^\(.*\)\.tar.*$/\1/g"`
-    if [ ! -d $SRC_DIR/$dir ]; then
-        mkdir -p $SRC_DIR
-	echo -e "Extract $dir"
-        tar -xf $DL_DIR/$1 -C $SRC_DIR
-    fi
-}
-
-function download_archive {
-    archive=`basename $1`
-    url=`echo $1 | sed -e "s/^\(.*\)\/.*$/\1/g"`
-    ver=`echo $1 | cut -d"-" -f2-`
-    if [ ! -f $DL_DIR/$archive ]; then
-        mkdir -p $DL_DIR
-	echo -e "Download $archive"
-        wget --spider -q $1 &&
-			wget -c $1 -O $DL_DIR/$archive || 
-			wget -c $url/v$ver -O $DL_DIR/$archive || exit 1
-    fi
-    extract_archive $archive
-}
 
 # Downloads
-
-download_archive https://www.landley.net/toybox/downloads/$TOYBOX_ARCHIVE
-
-## for qemu i686
-ARCH=i686
-TARGET=i686-pc-linux
-## for qemu PIC32
-# TARGET=mipsel-elf32
+download_archive https://www.landley.net/toybox/downloads $TOYBOX_NAME
 
 # toybox
 cd $BASEDIR
-mkdir -p $TGT_DIR/$TARGET/$TOYBOX_NAME/{build,rootfs}
 
-cd $TGT_DIR/$TARGET/$TOYBOX_NAME/build
+mkdir -p $TGT_DIR/$CROSS_TARGET/$TOYBOX_NAME/{build,rootfs}
+cd $TGT_DIR/$CROSS_TARGET/$TOYBOX_NAME/build
+
+DEFAULT_MAKE_OPTS="ARCH=$CROSS_ARCH CROSS_COMPILE=$CROSS_COMPILE $PARALLEL_BUILD_OPTS"
+
 if [ ! -f .configure ]; then
     cp -av $BASEDIR/configs/toybox.config $SRC_DIR/$TOYBOX_NAME/.config || die "Unable to place default config";
-    make $PARALLEL_BUILD_OPTS -C $SRC_DIR/$TOYBOX_NAME oldconfig  || die "Unable to initiate toybox";
+    make $DEFAULT_MAKE_OPTS -C $SRC_DIR/$TOYBOX_NAME oldconfig  || die "Unable to initiate toybox";
     touch .configure
 fi
 if [ ! -f .build ]; then
-    CFLAGS="--static" make $PARALLEL_BUILD_OPTS -C $SRC_DIR/$TOYBOX_NAME  toybox|| die "Unable to build toybox image";
+    CFLAGS="--static" make $DEFAULT_MAKE_OPTS -C $SRC_DIR/$TOYBOX_NAME  toybox|| die "Unable to build toybox image";
     touch .build
 fi
 if [ ! -f .install ]; then
-    rm -rf $TGT_DIR/$TARGET/$TOYBOX_NAME/rootfs || die "Unable to clean toybox rootfs";
+    rm -rf $TGT_DIR/$CROSS_TARGET/$TOYBOX_NAME/rootfs || die "Unable to clean toybox rootfs";
     
-    mkdir $TGT_DIR/$TARGET/$TOYBOX_NAME/rootfs || die "Unable to create toybox rootfs base";
-    make $PARALLEL_BUILD_OPTS -C $SRC_DIR/$TOYBOX_NAME PREFIX=$TGT_DIR/$TARGET/$TOYBOX_NAME/rootfs install || die "Unable to install toybox image";
+    mkdir $TGT_DIR/$CROSS_TARGET/$TOYBOX_NAME/rootfs || die "Unable to create toybox rootfs base";
+    make $DEFAULT_MAKE_OPTS -C $SRC_DIR/$TOYBOX_NAME PREFIX=$TGT_DIR/$CROSS_TARGET/$TOYBOX_NAME/rootfs install || die "Unable to install toybox image";
     touch .install
 fi
 if [ ! -f .archive ]; then
     archive=$SUBMARINE_BUILD_DIR/toybox.tar.gz
     rm -rf $archive
-    tar -C $TGT_DIR/$TARGET/$TOYBOX_NAME/ -cjf $archive rootfs || die "Unable to archive toybox install tree";
+    tar -C $TGT_DIR/$CROSS_TARGET/$TOYBOX_NAME/ -cjf $archive rootfs || die "Unable to archive toybox install tree";
     touch .archive
 fi
 
