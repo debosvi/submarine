4ba71a51745db5a25aec817725925616dd738666
Vincent de RIBOU <v.deribou@gmail.com>
2018-02-15 20:23:56 +0100

    ASYNCIO start.

---
+++ components/canbus/asyncio/files/lib/s6cb_asyncio_get_buffer.c
@@ -0,0 +1,8 @@
+/* ISC license. */
+
+#include <private/asyncio_p.h>
+
+int s6cb_asyncio_get_buffer(const s6cb_asyncio_id_t id, s6cb_asyncio_buffer_f f, char* const frame) {
+    
+    return S6CANBUS_ERROR_NONE;
+}
